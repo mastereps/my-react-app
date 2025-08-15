eb6efd534f30596cc89310ca5cf9e4c983ab3675
mastereps <efraim.gondraneos@gmail.com>
2025-08-16 06:35:33 +0800

    render list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,17 @@
 function ListGroup() {
-  return <h1>List Group</h1>;
+  const items = ["New York", "San Francisco", "Tokyo", "Shibuya"];
+
+  return (
+    <>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
