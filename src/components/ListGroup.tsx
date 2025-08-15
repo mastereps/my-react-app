94a4f351852ad1bc765158bee4fccbf678ed7f52
mastereps <efraim.gondraneos@gmail.com>
2025-08-16 07:14:43 +0800

    Event Handlers

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,22 @@
-import React from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "Shibuya"];
   // items = [];
 
-  function getMessage() {
-    return items.length === 0 && <p>No items found</p>;
-  }
+  const getMessage = () => items.length === 0 && <p>No items found</p>;
+
+  const handleClick = (e: MouseEvent) => console.log(e);
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {getMessage()}
-        {items.map((item) => (
-          <React.Fragment key={item}>
-            {" "}
-            <li onClick={() => console.log(item)} className="list-group-item">
-              {item}
-            </li>
-            <p>Hello World</p>
-          </React.Fragment>
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
